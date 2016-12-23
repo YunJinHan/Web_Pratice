ea160f99638a0d915b0b58f41c14cf348fbd9d7a
YunJinHan <yjh9234@naver.com>
2016-12-23 11:22:20 +0900

    lab1

---
+++ lab1/a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+	int tmp;
+	scanf("%d",&tmp);
+	if (tmp==1){
+		printf("yjh9234@naver.com\n");
+	} else if (tmp==2){
+		printf("010-3024-7034\n");
+	}
+	return 0;
+}
